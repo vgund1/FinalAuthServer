d36a4fbf4aa13ff85dfd85cb7c792e5ce6990581
vijay <vijay.gund@gmail.com>
2018-03-09 15:01:47 +0530

    Commit for war

--- resource-server/src/main/java/com/mdshkv/md/mediapp/oauth2/resourceserver/ResourceServer.java
+++ resource-server/src/main/java/com/mdshkv/md/mediapp/oauth2/resourceserver/ResourceServer.java
@@ -2,13 +2,20 @@ package com.mdshkv.md.mediapp.oauth2.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.mdshkv.md.mediapp.oauth2.resourceserver")
-public class ResourceServer {
+public class ResourceServer extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(ResourceServer.class);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    	return super.configure(builder);
     }
 }
--- resource-server/src/main/java/com/mdshkv/md/mediapp/oauth2/resourceserver/conf/ResourceServerTokenStoreConfig.java
+++ resource-server/src/main/java/com/mdshkv/md/mediapp/oauth2/resourceserver/conf/ResourceServerTokenStoreConfig.java
@@ -1,10 +1,5 @@
 package com.mdshkv.md.mediapp.oauth2.resourceserver.conf;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -19,35 +14,11 @@ public class ResourceServerTokenStoreConfig {
     public ResourceServerTokenServices remoteTokenServices() {
         RemoteTokenServices tokenService = new RemoteTokenServices();
         tokenService.setCheckTokenEndpointUrl(
-                "http://localhost:8443/auth-service/oauth/check_token");
+                "http://localhost:8080/auth-server-1.0-SNAPSHOT/oauth/check_token");
         tokenService.setClientId("web-client");
         tokenService.setClientSecret("web-client-secret");
         return tokenService;
     }
 
-	
-    
-    static {
-    TrustManager[] trustAllCerts = new TrustManager[]{
-    		new X509TrustManager() {
-    		    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-    		        return null;
-    		    }
-    		    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-    		    }
-    		    public void checkServerTrusted(
-    		        java.security.cert.X509Certificate[] certs, String authType) {
-    		    }
-    		}};
-
-    		   try {
-    		    SSLContext sc = SSLContext.getInstance("SSL");
-    		    sc.init(null, trustAllCerts, new java.security.SecureRandom());
-    		    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-    		    } catch (Exception e) {
-    		   
-    		    }
-}
-   
-   
+	   
 }
